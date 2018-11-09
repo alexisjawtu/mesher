9489930231e1795b7ec04aae99c8f9cded18cf75
alexisjawtu <ajawtu@dm.uba.ar>
2018-11-09 13:03:26 -0300

    bug with the plots of the sing vrtx macroel fixed. Beware of list comprehensions and surrounding variables.

--- plot_mesh.py
+++ plot_mesh.py
@@ -15,49 +15,54 @@ def plot_bbrick():
 	octants     = [6] # range(6,9) # any sublist in range(2,9)
 
 	for n in refinements:
+		print "1, ", n
 		# here macro_elems is 3, just one hybrid
-	    coords  = cube_mesh_2(n,mu[3],p_,macro_el,octants,macro_elems)
-	    # uncomment for just the second octant
-	    drawing = cube_drawing(coords,octants,macro_elems)
+		coords  = cube_mesh_2(n,mu[3],p_,macro_el,octants,macro_elems)
+		print "2, ", n
+		drawing = cube_drawing(coords,octants,macro_elems)
+		print "3, ", n
 	    # drawing = cube_drawing(coords)
-	    del(coords)
-	    fig = plt.figure()
-	    ax  = fig.add_subplot(1,1,1, projection='3d')
-    	
-	    elev = 30
-    	
-	    #for azim in [49, 79,109,139]:
-	    colors  = ['brown','darkgreen','red','black','fuchsia','blue']*7
-	    #random.shuffle(colors)
-	    for azim in angle_steps:
-	        c   = 0
-	        # ax.axis('equal')
-	    	# ax.set_xlim3d(0.1,-1.1)
-	    	# ax.set_ylim3d(-1.0,1.0)
-	    	# ax.set_zlim3d(-0.2,1.2)
-	        angle = 49 + 15*(azim-1)
-	        ax.view_init(elev, angle)
-	        for tetra in drawing:
-	            col_interval = int(len(tetra)/len(octants))
-	            for o in range(len(octants)):
-	                col = colors[c]
-	                c   = c + 1
-	                for dr in range(col_interval*o, col_interval*(1+o)):
-	                	## TODO: this can be done putting (array_of_X, array_of_Y, array_of_Z, ...)
-	                	## and not one by one as is now
-	                    ax.plot(tetra[dr][0],tetra[dr][1],tetra[dr][2],color = col)
+		del(coords)
+		print "4, ", n
+		fig = plt.figure()
+		ax  = fig.add_subplot(1,1,1, projection='3d')
+		
+		elev = 30
+		
+		#for azim in [49, 79,109,139]:
+		colors  = ['brown','darkgreen','red','black','fuchsia','blue']*7
+		#random.shuffle(colors)
+		for azim in angle_steps:
+			print "5, ", n
+			c   = 0
+			# ax.axis('equal')
+			# ax.set_xlim3d(0.1,-1.1)
+			# ax.set_ylim3d(-1.0,1.0)
+			# ax.set_zlim3d(-0.2,1.2)
+			angle = 49 + 15*(azim-1)
+			ax.view_init(elev, angle)
+			for tetra in drawing:
+				col_interval = int(len(tetra)/len(octants))
+				for o in range(len(octants)):
+					col = colors[c]
+					c   = c + 1
+					for dr in range(col_interval*o, col_interval*(1+o)):
+						print "6, ", n
+						## TODO: this can be done putting (array_of_X, array_of_Y, array_of_Z, ...)
+						## and not one by one as is now
+						ax.plot(tetra[dr][0],tetra[dr][1],tetra[dr][2],color = col)
 			# now cubic macro-els nr 0,1,2 and 4 with tetrahedra
 	        for oc in octants:
-	        	for m in [0]:
+	        	for m in [0,1,2,4]:
 					q 		= octant(oc, p_)
 					P0 	  	= q[:,macro_el[m,0]]
 					P1 	  	= q[:,macro_el[m,1]]
 					P2 	  	= q[:,macro_el[m,2]]
 					P3 	  	= q[:,macro_el[m,3]]
-					print '1 m: ', m
+					print "7, ", n
 					points_T5 = macroel_sing_vrtx(P0, P1, P2, P3, mu[m], n)
-					print '2 m: ', m
-					for i in [4*n for n in range(points_T5.shape[0]/4)]:
+					print "8, ", n
+					for i in [4*nn for nn in range(points_T5.shape[0]/4)]:
 						a = np.array([0,1,2,3]+[0,2,3,1])+i*np.ones(8,dtype=int)
 					
 						b = np.array([2]*8)
@@ -70,13 +75,13 @@ def plot_bbrick():
 						x = points_T5[a,b]
 						ax.plot(x, y, z)
 					del(points_T5)
-	        ax.plot([],[],[],label = "mu[3] = " + str(mu[3]) + str(macro_elems) + str(octants))
-	        legend = ax.legend()
-	        ax.set_xlabel(' X ')
-	        ax.set_ylabel(' Y ')
-	        ax.set_zlabel(' Z ')
-#	        plt.show()
-	        fig.savefig('bbrick' + str(azim) + '-' + str(n) + '.png')
+			ax.plot([],[],[],label = "mu[3] = " + str(mu[3]) + str(macro_elems) + str(octants))
+			legend = ax.legend()
+			ax.set_xlabel(' X ')
+			ax.set_ylabel(' Y ')
+			ax.set_zlabel(' Z ')
+			plt.show()
+			fig.savefig('bbrick' + str(azim) + '-' + str(n) + '.png')
 	return
 
 def plot_fichera():
