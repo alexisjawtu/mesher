a6830fc7dfc20aeb47737ad721dd1c1479774e6f
iojea <iojea@dm.uba.ar>
2019-07-01 16:31:05 -0300

    segment_mesh.py plots poliedral octant

--- mesh_segment.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-
-from numpy import linspace, pi, cos, sin, array
--- segment_mesh.py
+++ segment_mesh.py
@@ -10,16 +10,23 @@ def build_pill(mu,levels):
 	vert_tetra = np.array([[0,0,0.5],[1,0,0.5],[0,1,0.5],[0,0,1.5]]).transpose()
 	points = mesh.macroel_tetrahedra(vert_tetra,mu,levels)
 	
-	vert_prism = np.array([[0,0,0.5],[0,0,0],[1,0,0],[1,0,0.5]],[0,1,0],[0,1,0.5]).transpose()
+	vert_prism = np.array([[0,0,0.5],[0,0,0],[1,0,0],[1,0,0.5],[0,1,0],[0,1,0.5]])
 	nodos = macro_prism(vert_prism,mu,levels)
 	nodos.update({'t0':points})
 
-	pbc_tetra(plt,nodos['t0'],'blue')
-	pbc_tetra(plt,nodos['t1'],'green')
-	pbc_tetra(plt,nodos['t2'],'green')
-	pbc_hybrid(plt,nodos['tp'],'black')
+	fig = plt.figure()
+	ax  = fig.add_subplot(1,1,1,projection='3d')
+	ax.plot([],[],[],label = " ")
+	legend = ax.legend()
+	ax.set_xlabel(' X ')
+	ax.set_ylabel(' Y ')
+	ax.set_zlabel(' Z ')
 
-	pbc_tetra(plt,points,_color)
+	pbc_tetra(ax,nodos['t0'],'blue')
+	pbc_tetra(ax,nodos['t1'],'green')
+	pbc_tetra(ax,nodos['t2'],'green')
+	pbc_hybrid(ax,nodos['tp'],'black')
+	
 	plt.show()
 	#p_new = []
 	#for k in range(points.shape[0]):
@@ -40,12 +47,13 @@ def build_pill(mu,levels):
 
 def macro_prism(vertices,mu,levels):
 	dict_out = {}
+	print(vertices.shape)
 	t1_tetra = vertices[[0,2,3,4],:]
 	t2_tetra = vertices[[0,3,4,5],:]
 	t_prism = vertices[[1,2,4,0],:]
 	points_t1 = mesh.macroel_tetrahedra(t1_tetra.transpose(),mu,levels)
 	points_t2 = mesh.macroel_tetrahedra(t2_tetra.transpose(),mu,levels)
-	points_t_p = mesh.macro_hybrid(t_prism.transpose(),mu,levels)
+	points_t_p = mesh.macroel_hybrid(t_prism.transpose(),mu,levels)
 	dict_out['tp'] = points_t_p
 	dict_out['t1'] = points_t1
 	dict_out['t2'] = points_t2
@@ -54,45 +62,45 @@ def macro_prism(vertices,mu,levels):
 def pbc_hybrid(ax,points,_color):
 	drawing = []
 	n = points.shape[0] - 1
-    for k in range (n+1):
-        for i in range (n-k+1):
-            for j in range (n-k-i+1):
-                x, y, z = points[k,0:n-k-j+1,0,j], points[k,0:n-k-j+1,1,j], points[k,0:n-k-j+1,2,j]
-                drawing.append([x,y,z])
-            x, y, z = points[k,i,0,0:n-k-i+1], points[k,i,1,0:n-k-i+1], points[k,i,2,0:n-k-i+1]
-            drawing.append([x,y,z])
-        for c in range (n-k+1):
-            x, y, z = np.zeros(c+1), np.zeros(c+1), np.zeros(c+1)
-            for i in range (c+1):
-                x[i] = points[k,i,0,c-i]
-                y[i] = points[k,i,1,c-i]
-                z[i] = points[k,i,2,c-i]
-            drawing.append([x,y,z])  #  transversals
-    for i in range (n):
-        for j in range (n-i):
-            stop = n - (i + j) + 1
-            x = points[0:stop,i,0,j]
-            y = points[0:stop,i,1,j]
-            z = points[0:stop,i,2,j]
-            drawing.append([x,y,z])  #  verticals
-        x, y, z = np.zeros(n-i+1), np.zeros(n-i+1), np.zeros(n-i+1)
-        for k in range (n-i+1):
-            x[k] = points[k,i,0,n-k-i]
-            y[k] = points[k,i,1,n-k-i]
-            z[k] = points[k,i,2,n-k-i]
-        drawing.append([x,y,z])  #  pyramidals
-    # simply interchange the roles of i and j
-        x, y, z = np.zeros(n-i+1), np.zeros(n-i+1), np.zeros(n-i+1)
-        for k in range (n-i+1):
-            x[k] = points[k,n-k-i,0,i]
-            y[k] = points[k,n-k-i,1,i]
-            z[k] = points[k,n-k-i,2,i]
-        drawing.append([x,y,z])  #  pyramidals
-    for dr in range(len(drawing)):
-        ## TODO: this can be done putting (array_of_X, array_of_Y, array_of_Z, ...)
-        ## and not one by one as is now
-        ax.plot(drawing[dr][0],drawing[dr][1],drawing[dr][2], color = _color)
-    return
+	for k in range (n+1):
+		for i in range (n-k+1):
+			for j in range (n-k-i+1):
+				x, y, z = points[k,0:n-k-j+1,0,j], points[k,0:n-k-j+1,1,j], points[k,0:n-k-j+1,2,j]
+				drawing.append([x,y,z])
+			x, y, z = points[k,i,0,0:n-k-i+1], points[k,i,1,0:n-k-i+1], points[k,i,2,0:n-k-i+1]
+			drawing.append([x,y,z])
+		for c in range (n-k+1):
+			x, y, z = np.zeros(c+1), np.zeros(c+1), np.zeros(c+1)
+			for i in range (c+1):
+				x[i] = points[k,i,0,c-i]
+				y[i] = points[k,i,1,c-i]
+				z[i] = points[k,i,2,c-i]
+			drawing.append([x,y,z])  #  transversals
+	for i in range (n):
+		for j in range (n-i):
+			stop = n - (i + j) + 1
+			x = points[0:stop,i,0,j]
+			y = points[0:stop,i,1,j]
+			z = points[0:stop,i,2,j]
+			drawing.append([x,y,z])  #  verticals
+		x, y, z = np.zeros(n-i+1), np.zeros(n-i+1), np.zeros(n-i+1)
+		for k in range (n-i+1):
+			x[k] = points[k,i,0,n-k-i]
+			y[k] = points[k,i,1,n-k-i]
+			z[k] = points[k,i,2,n-k-i]
+		drawing.append([x,y,z])  #  pyramidals
+	# simply interchange the roles of i and j
+		x, y, z = np.zeros(n-i+1), np.zeros(n-i+1), np.zeros(n-i+1)
+		for k in range (n-i+1):
+			x[k] = points[k,n-k-i,0,i]
+			y[k] = points[k,n-k-i,1,i]
+			z[k] = points[k,n-k-i,2,i]
+		drawing.append([x,y,z])  #  pyramidals
+	for dr in range(len(drawing)):
+		## TODO: this can be done putting (array_of_X, array_of_Y, array_of_Z, ...)
+		## and not one by one as is now
+		ax.plot(drawing[dr][0],drawing[dr][1],drawing[dr][2], color = _color)
+	return
 
 
 def pbc_tetra(ax,points,_color):
@@ -105,7 +113,7 @@ def pbc_tetra(ax,points,_color):
 	
 
 
-def pbc_all(data)
+def pbc_all(data):
 	fig = plt.figure()
 	ax  = fig.add_subplot(1,1,1,projection='3d')
 	ax.plot([],[],[],label = " ")
